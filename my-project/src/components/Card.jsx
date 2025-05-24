11d5d3fce00b7188dc742d2db9fb8487c768172a
Piyush <pbajpai207@gmail.com>
2025-05-24 23:40:48 +0530

    fix minor bug

--- my-project/src/components/Card.jsx
+++ my-project/src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = () => {
   return (
     <div className="card">
       <img src={weather?.data?.current?.condition?.icon} alt="mypic" />
-        <h2>{weather.data?.current?.temp_c}°C</h2>
+        <h2>{weather.data?.current?.temp_c}</h2>
         <h5>{weather.data?.location?.name}, {weather.data?.location?.region}, {weather.data?.location?.country}
         </h5>
     </div>
